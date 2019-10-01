0a6b6647713876c0dc03901ae5625878a7808b41
Eric Schles <eric.schles@sema4genomics.com>
2019-10-01 11:03:59 -0400

    adding correct timezone

--- etl_question/app.py
+++ etl_question/app.py
@@ -1,19 +1,26 @@
 from flask import Flask
 import numpy as np
 from datetime import datetime
+import dateparser
 import json
 
 app = Flask(__name__)
     
 @app.route("/", methods=["GET"])
 def index():
+    settings = {
+        'TIMEZONE': 'UTC',
+        'TO_TIMEZONE': 'US/Eastern'
+    }
     center = 3
     spread = 5
     return json.dumps({
         "A": np.random.normal(loc=center, scale=spread),
         "B": np.random.normal(loc=center, scale=spread),
         "C": np.random.normal(loc=center, scale=spread),
-        "timestamp_one": str(datetime.now()),
+        "timestamp_one": dateparser.parse(
+            str(datetime.now()), settings=settings
+        ),
         "timestamp_two": str(datetime.utcnow())
     })
 
